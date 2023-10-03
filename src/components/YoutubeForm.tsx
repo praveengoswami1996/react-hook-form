4b35825579bb03278770860e46f2fc27a7feb4aa
Praveen Goswami <praveen828589@gmail.com>
2023-10-04 00:25:46 +0530

    Added more react-hook-form concepts

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,5 +1,8 @@
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
+//useFieldArray is used for handling Dynamic Form Fields
+
 import { DevTool } from '@hookform/devtools';
+import { useEffect } from "react";
 
 type FormValues = {
   username: string
@@ -10,8 +13,17 @@ type FormValues = {
     twitter: string
     facebook: string
   }
-  //Input Fields as Arrays
-  phoneNumbers: string[]
+  //Input Fields as Arrays of String
+  phoneNumbers: string[];
+
+  //Input Fields as Array of Objects (useFieldArray Example)
+  phNumbers: {
+    number: string;
+  }[];
+  
+  //How to deal with numeric and date values
+  age: number;
+  dob: Date;
 }
 
 const YoutubeForm = () => {
@@ -20,27 +32,37 @@ const YoutubeForm = () => {
       username: "",
       email: "",
       channel: "",
-      //Form Fields as Nested Objects
       social: {
         twitter: "",
         facebook: ""
       },
-      phoneNumbers: ["", ""]
+      phoneNumbers: ["", ""],
+      phNumbers: [{ number: '' }],
+      age: 0,
+      dob: new Date(),
     }
     // Setting default values by fetching data from an API
-
-    // defaultValues: async () => {
-    //   const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
-    //   const data = await response.json();
-    //   return {
-    //     username: data.name,
-    //     email: data.email,
-    //     channel: ""
-    //   }
-    // }
+    /*
+      defaultValues: async () => {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+        const data = await response.json();
+        return {
+          username: data.name,
+          email: data.email,
+          channel: ""
+        }
+      }
+    */
+    
   });
-  const { register, control, handleSubmit, formState } = form;
+  const { register, control, handleSubmit, watch, formState } = form;
   const { errors } = formState;
+
+  //useFieldArray returns an array of fields which we can render in the JSX
+  const { fields, append, remove } = useFieldArray({
+    name: 'phNumbers',
+    control: control
+  })
 
   console.log(errors);
 
@@ -49,10 +71,37 @@ const YoutubeForm = () => {
   const onSubmit = (data: FormValues) => {
     console.log("Form Submitted", data);
   }
+
+  /************** Watching Field Values Starts *******************/
+  const watchUsername = watch('username');
+  //const watchMultiple = watch(['username', 'email']);
+  //const watchEntireForm = watch();
+
+  /*
+  When we start typing into the form fields, watch() continuously observes the form fields
+  values and updates the UI.Because of this the component re-renders everytime the watched value changes. This is fine for the small projects like this.
+
+  On the other hand, we can perform a side effect after watching a value, the we can use the "callback version of the watch method". It will prevent the re-rendering on changing field values. 
+  
+  */
+
+  useEffect(() => {
+    //the function receives the updated value as argument
+    const subscription = watch((updatedFieldValue) => {
+      console.log(updatedFieldValue);
+    })
+
+    return () => subscription.unsubscribe();
+  }, [watch])
+
+  /******************Watching Field Values Ends*********************/
+  
+
   
   return (
     <div className="form-container">
         <h1><span className="heading-text-1">React</span> <span className="heading-text-2">Hook</span> <span className="heading-text-3">Form</span> <span className="heading-text-4">Tutorial</span></h1>
+        <h2>Watched Value: {watchUsername}</h2>
 
         <form onSubmit={handleSubmit(onSubmit)} noValidate>
             
@@ -90,12 +139,16 @@ const YoutubeForm = () => {
                     message: "Invalid email format"
                   },
                   //Custom Validations 
+                  //This validate function automatically receives fieldValue
+                  /*
+                    validate: (fieldValue) => {
+                      return (
+                        fieldValue !== 'admin@example.com' || 'Enter a different email address'
+                      ); 
+                    }
+                  */ 
 
-                  // validate: (fieldValue) => {
-                  //   return (
-                  //     fieldValue !== 'admin@example.com' || 'Enter a different email address'
-                  //   ); 
-                  // } 
+                  // validate can also be written as an object in which each key is the name of a validation rule
                   validate: {
                     notAdmin: (fieldValue) => {
                       return (
@@ -201,6 +254,78 @@ const YoutubeForm = () => {
               <p className="error">{errors.phoneNumbers?.[1]?.message}</p>
             </div>
             {/* Input Fields as Array Ends */}
+
+            {/* useFieldArray Example Starts */}
+            <div>
+                <label>List of Phone Numbers</label>
+                <div>
+                  {
+                    fields.map((field, index) => {
+                      return (
+                        <div className="form-control" key={field.id}>
+                          <input 
+                            type="text"
+                            {...register(`phNumbers.${index}.number` as const)}
+                          />
+                          {
+                            index > 0 && (
+                              <button 
+                                type="button" 
+                                onClick={() => remove(index)}
+                              >
+                                Remove
+                              </button>
+                            )
+                          }
+                        </div>
+                      )
+                    })
+                  }
+                  <button 
+                    type="button" 
+                    onClick={() => append({ number: "" })}
+                  >
+                    Add Phone Number
+                  </button>
+                </div>
+            </div>
+            {/* useFieldArray Example Ends */}
+
+            {/* Numeric Input Example Starts */}
+            <div className="form-control">
+              <label htmlFor="age">age</label>
+              <input 
+                type="number" 
+                id="age" 
+                {...register("age", {
+                  valueAsNumber: true, //to get number as number not string
+                  required: {
+                    value: true,
+                    message: "Age is required"
+                  }
+                })}
+              />
+              <p className="error">{errors.age?.message}</p>
+            </div>
+            {/* Numeric Input Example Ends */}
+
+            {/* Date Input Example Starts */}
+            <div className="form-control">
+              <label htmlFor="dob">Date of birth</label>
+              <input 
+                type="date" 
+                id="dob" 
+                {...register("dob", {
+                  valueAsDate: true, //to get date as date type value not string
+                  required: {
+                    value: true,
+                    message: "DOB is required"
+                  }
+                })}
+              />
+              <p className="error">{errors.dob?.message}</p>
+            </div>
+            {/* Date Input Example Ends */}
 
             <button>Submit</button>
         </form>
